3c2c9037b3eb9d58bcdc0b64e427e73e03c6fdab
Taylor Wilson <twilson@flmnh.ufl.edu>
2019-10-15 14:47:16 -0400

    initial commit

---
+++ SpecifyPrepAdd/SpecifyPrepAdd/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace SpecifyPrepAdd
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MySQLUser = new System.Windows.Forms.TextBox();
+            this.MySQLHost = new System.Windows.Forms.TextBox();
+            this.MySQLDb = new System.Windows.Forms.TextBox();
+            this.messageBox = new System.Windows.Forms.TextBox();
+            this.MySQLPasswordLabel = new System.Windows.Forms.Label();
+            this.MySQLUsernameLabel = new System.Windows.Forms.Label();
+            this.MySQLHostLabel = new System.Windows.Forms.Label();
+            this.MySQLDBLabel = new System.Windows.Forms.Label();
+            this.PrepTypeCombobox = new System.Windows.Forms.ComboBox();
+            this.PrepTypeLabel = new System.Windows.Forms.Label();
+            this.MySQLPass = new System.Windows.Forms.TextBox();
+            this.SelectedCSVTextBox = new System.Windows.Forms.TextBox();
+            this.SelectCSVButton = new System.Windows.Forms.Button();
+            this.CSVFileLabel = new System.Windows.Forms.Label();
+            this.CSVDataGrid = new System.Windows.Forms.DataGridView();
+            this.messageBoxLabel = new System.Windows.Forms.Label();
+            this.PrepTypeBox = new System.Windows.Forms.GroupBox();
+            this.addPrepsButton = new System.Windows.Forms.Button();
+            this.collectionBox = new System.Windows.Forms.GroupBox();
+            this.collectionLabel = new System.Windows.Forms.Label();
+            this.collectionComboBox = new System.Windows.Forms.ComboBox();
+            this.connectButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.CSVDataGrid)).BeginInit();
+            this.PrepTypeBox.SuspendLayout();
+            this.collectionBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MySQLUser
+            // 
+            this.MySQLUser.Location = new System.Drawing.Point(47, 38);
+            this.MySQLUser.Name = "MySQLUser";
+            this.MySQLUser.Size = new System.Drawing.Size(226, 20);
+            this.MySQLUser.TabIndex = 0;
+            // 
+            // MySQLHost
+            // 
+            this.MySQLHost.Location = new System.Drawing.Point(47, 127);
+            this.MySQLHost.Name = "MySQLHost";
+            this.MySQLHost.Size = new System.Drawing.Size(226, 20);
+            this.MySQLHost.TabIndex = 2;
+            // 
+            // MySQLDb
+            // 
+            this.MySQLDb.Location = new System.Drawing.Point(321, 127);
+            this.MySQLDb.Name = "MySQLDb";
+            this.MySQLDb.Size = new System.Drawing.Size(226, 20);
+            this.MySQLDb.TabIndex = 3;
+            // 
+            // messageBox
+            // 
+            this.messageBox.Location = new System.Drawing.Point(587, 38);
+            this.messageBox.Multiline = true;
+            this.messageBox.Name = "messageBox";
+            this.messageBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.messageBox.Size = new System.Drawing.Size(596, 386);
+            this.messageBox.TabIndex = 4;
+            // 
+            // MySQLPasswordLabel
+            // 
+            this.MySQLPasswordLabel.AutoSize = true;
+            this.MySQLPasswordLabel.Location = new System.Drawing.Point(318, 22);
+            this.MySQLPasswordLabel.Name = "MySQLPasswordLabel";
+            this.MySQLPasswordLabel.Size = new System.Drawing.Size(91, 13);
+            this.MySQLPasswordLabel.TabIndex = 5;
+            this.MySQLPasswordLabel.Text = "MySQL Password";
+            // 
+            // MySQLUsernameLabel
+            // 
+            this.MySQLUsernameLabel.AutoSize = true;
+            this.MySQLUsernameLabel.Location = new System.Drawing.Point(44, 22);
+            this.MySQLUsernameLabel.Name = "MySQLUsernameLabel";
+            this.MySQLUsernameLabel.Size = new System.Drawing.Size(93, 13);
+            this.MySQLUsernameLabel.TabIndex = 6;
+            this.MySQLUsernameLabel.Text = "MySQL Username";
+            // 
+            // MySQLHostLabel
+            // 
+            this.MySQLHostLabel.AutoSize = true;
+            this.MySQLHostLabel.Location = new System.Drawing.Point(44, 111);
+            this.MySQLHostLabel.Name = "MySQLHostLabel";
+            this.MySQLHostLabel.Size = new System.Drawing.Size(67, 13);
+            this.MySQLHostLabel.TabIndex = 7;
+            this.MySQLHostLabel.Text = "MySQL Host";
+            // 
+            // MySQLDBLabel
+            // 
+            this.MySQLDBLabel.AutoSize = true;
+            this.MySQLDBLabel.Location = new System.Drawing.Point(318, 111);
+            this.MySQLDBLabel.Name = "MySQLDBLabel";
+            this.MySQLDBLabel.Size = new System.Drawing.Size(91, 13);
+            this.MySQLDBLabel.TabIndex = 8;
+            this.MySQLDBLabel.Text = "MySQL Database";
+            // 
+            // PrepTypeCombobox
+            // 
+            this.PrepTypeCombobox.FormattingEnabled = true;
+            this.PrepTypeCombobox.Location = new System.Drawing.Point(6, 68);
+            this.PrepTypeCombobox.Name = "PrepTypeCombobox";
+            this.PrepTypeCombobox.Size = new System.Drawing.Size(350, 21);
+            this.PrepTypeCombobox.TabIndex = 9;
+            // 
+            // PrepTypeLabel
+            // 
+            this.PrepTypeLabel.AutoSize = true;
+            this.PrepTypeLabel.Location = new System.Drawing.Point(6, 52);
+            this.PrepTypeLabel.Name = "PrepTypeLabel";
+            this.PrepTypeLabel.Size = new System.Drawing.Size(56, 13);
+            this.PrepTypeLabel.TabIndex = 10;
+            this.PrepTypeLabel.Text = "Prep Type";
+            // 
+            // MySQLPass
+            // 
+            this.MySQLPass.Location = new System.Drawing.Point(321, 38);
+            this.MySQLPass.Name = "MySQLPass";
+            this.MySQLPass.PasswordChar = '*';
+            this.MySQLPass.Size = new System.Drawing.Size(226, 20);
+            this.MySQLPass.TabIndex = 1;
+            // 
+            // SelectedCSVTextBox
+            // 
+            this.SelectedCSVTextBox.Location = new System.Drawing.Point(6, 29);
+            this.SelectedCSVTextBox.Name = "SelectedCSVTextBox";
+            this.SelectedCSVTextBox.Size = new System.Drawing.Size(350, 20);
+            this.SelectedCSVTextBox.TabIndex = 12;
+            this.SelectedCSVTextBox.TextChanged += new System.EventHandler(this.SelectedCSVTextBox_Click);
+            // 
+            // SelectCSVButton
+            // 
+            this.SelectCSVButton.Location = new System.Drawing.Point(362, 29);
+            this.SelectCSVButton.Name = "SelectCSVButton";
+            this.SelectCSVButton.Size = new System.Drawing.Size(144, 23);
+            this.SelectCSVButton.TabIndex = 13;
+            this.SelectCSVButton.Text = "Select CSV File";
+            this.SelectCSVButton.UseVisualStyleBackColor = true;
+            this.SelectCSVButton.Click += new System.EventHandler(this.SelectCSVButton_Click);
+            // 
+            // CSVFileLabel
+            // 
+            this.CSVFileLabel.AutoSize = true;
+            this.CSVFileLabel.Location = new System.Drawing.Point(3, 16);
+            this.CSVFileLabel.Name = "CSVFileLabel";
+            this.CSVFileLabel.Size = new System.Drawing.Size(65, 13);
+            this.CSVFileLabel.TabIndex = 14;
+            this.CSVFileLabel.Text = "CSV Upload";
+            // 
+            // CSVDataGrid
+            // 
+            this.CSVDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.CSVDataGrid.Location = new System.Drawing.Point(47, 430);
+            this.CSVDataGrid.Name = "CSVDataGrid";
+            this.CSVDataGrid.Size = new System.Drawing.Size(1136, 513);
+            this.CSVDataGrid.TabIndex = 15;
+            // 
+            // messageBoxLabel
+            // 
+            this.messageBoxLabel.AutoSize = true;
+            this.messageBoxLabel.Location = new System.Drawing.Point(584, 22);
+            this.messageBoxLabel.Name = "messageBoxLabel";
+            this.messageBoxLabel.Size = new System.Drawing.Size(55, 13);
+            this.messageBoxLabel.TabIndex = 17;
+            this.messageBoxLabel.Text = "Messages";
+            // 
+            // PrepTypeBox
+            // 
+            this.PrepTypeBox.Controls.Add(this.addPrepsButton);
+            this.PrepTypeBox.Controls.Add(this.SelectedCSVTextBox);
+            this.PrepTypeBox.Controls.Add(this.SelectCSVButton);
+            this.PrepTypeBox.Controls.Add(this.CSVFileLabel);
+            this.PrepTypeBox.Controls.Add(this.PrepTypeLabel);
+            this.PrepTypeBox.Controls.Add(this.PrepTypeCombobox);
+            this.PrepTypeBox.Location = new System.Drawing.Point(47, 312);
+            this.PrepTypeBox.Name = "PrepTypeBox";
+            this.PrepTypeBox.Size = new System.Drawing.Size(534, 112);
+            this.PrepTypeBox.TabIndex = 18;
+            this.PrepTypeBox.TabStop = false;
+            this.PrepTypeBox.Visible = false;
+            // 
+            // addPrepsButton
+            // 
+            this.addPrepsButton.Location = new System.Drawing.Point(362, 66);
+            this.addPrepsButton.Name = "addPrepsButton";
+            this.addPrepsButton.Size = new System.Drawing.Size(144, 23);
+            this.addPrepsButton.TabIndex = 15;
+            this.addPrepsButton.Text = "Add Preps";
+            this.addPrepsButton.UseVisualStyleBackColor = true;
+            this.addPrepsButton.Click += new System.EventHandler(this.addPrepsButton_Click);
+            // 
+            // collectionBox
+            // 
+            this.collectionBox.Controls.Add(this.collectionLabel);
+            this.collectionBox.Controls.Add(this.collectionComboBox);
+            this.collectionBox.Location = new System.Drawing.Point(47, 242);
+            this.collectionBox.Name = "collectionBox";
+            this.collectionBox.Size = new System.Drawing.Size(534, 64);
+            this.collectionBox.TabIndex = 19;
+            this.collectionBox.TabStop = false;
+            this.collectionBox.Visible = false;
+            // 
+            // collectionLabel
+            // 
+            this.collectionLabel.AutoSize = true;
+            this.collectionLabel.Location = new System.Drawing.Point(6, 13);
+            this.collectionLabel.Name = "collectionLabel";
+            this.collectionLabel.Size = new System.Drawing.Size(86, 13);
+            this.collectionLabel.TabIndex = 1;
+            this.collectionLabel.Text = "Select Collection";
+            // 
+            // collectionComboBox
+            // 
+            this.collectionComboBox.FormattingEnabled = true;
+            this.collectionComboBox.Location = new System.Drawing.Point(6, 29);
+            this.collectionComboBox.Name = "collectionComboBox";
+            this.collectionComboBox.Size = new System.Drawing.Size(350, 21);
+            this.collectionComboBox.TabIndex = 0;
+            this.collectionComboBox.SelectionChangeCommitted += new System.EventHandler(this.collectionComboBox_SelectionChangeCommitted);
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(409, 166);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(138, 23);
+            this.connectButton.TabIndex = 20;
+            this.connectButton.Text = "Connect to DB";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1260, 900);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.collectionBox);
+            this.Controls.Add(this.PrepTypeBox);
+            this.Controls.Add(this.messageBoxLabel);
+            this.Controls.Add(this.CSVDataGrid);
+            this.Controls.Add(this.MySQLDBLabel);
+            this.Controls.Add(this.MySQLHostLabel);
+            this.Controls.Add(this.MySQLUsernameLabel);
+            this.Controls.Add(this.MySQLPasswordLabel);
+            this.Controls.Add(this.messageBox);
+            this.Controls.Add(this.MySQLDb);
+            this.Controls.Add(this.MySQLHost);
+            this.Controls.Add(this.MySQLPass);
+            this.Controls.Add(this.MySQLUser);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.CSVDataGrid)).EndInit();
+            this.PrepTypeBox.ResumeLayout(false);
+            this.PrepTypeBox.PerformLayout();
+            this.collectionBox.ResumeLayout(false);
+            this.collectionBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox MySQLUser;
+        private System.Windows.Forms.TextBox MySQLHost;
+        private System.Windows.Forms.TextBox MySQLDb;
+        private System.Windows.Forms.TextBox messageBox;
+        private System.Windows.Forms.Label MySQLPasswordLabel;
+        private System.Windows.Forms.Label MySQLUsernameLabel;
+        private System.Windows.Forms.Label MySQLHostLabel;
+        private System.Windows.Forms.Label MySQLDBLabel;
+        private System.Windows.Forms.ComboBox PrepTypeCombobox;
+        private System.Windows.Forms.Label PrepTypeLabel;
+        private System.Windows.Forms.TextBox MySQLPass;
+        private System.Windows.Forms.TextBox SelectedCSVTextBox;
+        private System.Windows.Forms.Button SelectCSVButton;
+        private System.Windows.Forms.Label CSVFileLabel;
+        private System.Windows.Forms.DataGridView CSVDataGrid;
+        private System.Windows.Forms.Label messageBoxLabel;
+        private System.Windows.Forms.GroupBox PrepTypeBox;
+        private System.Windows.Forms.GroupBox collectionBox;
+        private System.Windows.Forms.Label collectionLabel;
+        private System.Windows.Forms.ComboBox collectionComboBox;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Button addPrepsButton;
+    }
+}
+
---
+++ SpecifyPrepAdd/SpecifyPrepAdd/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace SpecifyPrepAdd
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            CSVDataGrid.DataSource = dt.DefaultView;
+        }
+
+        private MySqlConnection GetMySqlConnection()
+        {
+            try
+            {
+                MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
+                builder.Server = MySQLHost.Text;
+                builder.Database = MySQLDb.Text;
+                builder.AllowZeroDateTime = true;
+                builder.Port = 3306;
+                builder.UserID = MySQLUser.Text;
+                builder.Password = MySQLPass.Text;
+                return new MySqlConnection(builder.ConnectionString);
+            }
+            catch (Exception e)
+            {
+                messageBox.AppendText(e.ToString() + "\n");
+                return null;
+            }
+           
+        }
+
+        private void PopulatePrepTypeButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MySqlConnection conn = GetMySqlConnection();
+
+            }
+            catch (Exception exc)
+            {
+                messageBox.AppendText(exc.ToString() + "\n");
+            }
+        }
+
+        private DataTable readCSV(string filePath)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                // create columns
+                File.ReadLines(filePath).Take(1)
+                    .SelectMany(x => x.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                    .ToList()
+                    .ForEach(x => dt.Columns.Add(x.Trim()));
+
+                // add the rows
+                File.ReadLines(filePath).Skip(1)
+                    .Select(x => x.Split(','))
+                    .ToList()
+                    .ForEach(line => dt.Rows.Add(line));
+            }
+            catch (Exception exc)
+            {
+                messageBox.AppendText(exc.ToString() + "\n");
+            }
+            return dt;
+        }
+
+        private DataSet GetTableDataSet(string tablename)
+        {
+            try
+            {
+                using (MySqlConnection conn = GetMySqlConnection())
+                {
+                    string select = String.Format("SELECT * FROM {0}", tablename);
+                    MySqlDataAdapter da = new MySqlDataAdapter(select, conn);
+                    MySqlCommandBuilder cb = new MySqlCommandBuilder(da);
+                    DataSet ds = new DataSet();
+                    da.Fill(ds, tablename);
+                    return ds;
+                }
+            }
+            catch (Exception exc)
+            {
+                messageBox.AppendText(exc.ToString() + "\n");
+                return null;
+            }
+        }
+
+        private List<string> getCollections()
+        {
+            try
+            {
+                DataSet ds = GetTableDataSet("collection");
+                DataTable dt = ds.Tables["collection"];
+                var collections = (from DataRow row in dt.Rows
+                                   select row["CollectionName"].ToString());
+                return collections.ToList();
+            }
+            catch (Exception exc)
+            {
+                messageBox.AppendText(exc.ToString() + "\n");
+                return null;
+            }
+        }
+
+        private List<string> getPrepTypes(string collection)
+        {
+            try
+            {
+                using (MySqlConnection conn = GetMySqlConnection())
+                {
+                    string select = String.Format("SELECT Name FROM preptype INNER JOIN collection USING(CollectionID) WHERE CollectionName = '{0}'", collection);
+                    MySqlDataAdapter da = new MySqlDataAdapter(select, conn);
+                    MySqlCommandBuilder cb = new MySqlCommandBuilder(da);
+                    DataSet ds = new DataSet();
+                    da.Fill(ds, "preptype");
+                    var preptypes = (from DataRow row in ds.Tables["preptype"].Rows
+                                     select row["Name"].ToString());
+                    return preptypes.ToList();
+                }
+            }
+            catch (Exception exc)
+            {
+                messageBox.AppendText(exc.ToString() + "\n");
+                return null;
+            }
+        }
+
+        private int GetCollectionObjectID(string guid)
+        {
+            try
+            {
+                using(MySqlConnection conn = GetMySqlConnection())
+                {
+                    string sql = @"SELECT CollectionObjectID FROM collectionobject WHERE GUID LIKE @guid";
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
+                    cmd.Parameters.Add("@guid", MySqlDbType.VarChar);
+                    cmd.Parameters["@guid"].Value = guid;
+                    conn.Open();
+                    int collectionObjectID = (int)cmd.ExecuteScalar();
+                    conn.Close();
+                    return collectionObjectID;
+                }
+            }
+            catch(Exception exc)
+            {
+                messageBox.AppendText(exc.ToString() + "\n");
+                return 0;
+            }
+        }
+
+        private void SelectCSVButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDlg = new OpenFileDialog();
+            if (openFileDlg.ShowDialog() == DialogResult.OK)
+            {
+                SelectedCSVTextBox.Text = openFileDlg.FileName;
+            }
+        }
+
+        private void SelectedCSVTextBox_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                CSVDataGrid.DataSource = readCSV(SelectedCSVTextBox.Text).DefaultView;
+            }
+            catch (Exception exc)
+            {
+                messageBox.AppendText(exc.ToString() + "\n");
+            }
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (MySqlConnection conn = GetMySqlConnection())
+                {
+                    conn.Open();
+                    if (conn.State == ConnectionState.Open)
+                    {
+                        collectionComboBox.DataSource = getCollections();
+                        collectionBox.Visible = true;
+                    }
+                }
+            }
+            catch (Exception exc)
+            {
+                messageBox.AppendText(exc.ToString() + "\n");
+            }
+        }
+
+        private void collectionComboBox_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            try
+            {
+                PrepTypeCombobox.DataSource = getPrepTypes(collectionComboBox.Text);
+                PrepTypeBox.Visible = true;
+            }
+            catch (Exception exc)
+            {
+                messageBox.AppendText(exc.ToString() + "\n");
+            }
+        }
+
+        private void addPrepsButton_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            try
+            {
+                DataSet prepTypes = GetTableDataSet("preptype");
+                var pts = (from DataRow preprow in prepTypes.Tables["preptype"].AsEnumerable()
+                           where (string)preprow["Name"] == PrepTypeCombobox.Text
+                           select preprow);
+                int prepTypeID = (int)pts.First()["PrepTypeID"];
+                DataSet collections = GetTableDataSet("collection");
+                var coll = (from DataRow collrow in collections.Tables["collection"].AsEnumerable()
+                            where (string)collrow["CollectionName"] == collectionComboBox.Text
+                            select collrow);
+                int collectionID = (int)coll.First()["CollectionID"];
+                using (MySqlConnection conn = GetMySqlConnection())
+                {
+                    foreach (DataGridViewRow row in CSVDataGrid.Rows)
+                    {
+                        if (row.Cells[0].Value != null || row.Cells[0].Value != DBNull.Value || !String.IsNullOrWhiteSpace(row.Cells[0].Value.ToString()))
+                        {
+                            int collectionObjectID = GetCollectionObjectID(row.Cells[0].Value.ToString());
+                            string sql = @"INSERT INTO preparation (TimestampCreated, 
+                                                            Version, 
+                                                            CollectionMemberID, 
+                                                            CountAmt,
+                                                            CollectionObjectID, 
+                                                            CreatedByAgentID, 
+                                                            PrepTypeID, 
+                                                            GUID)
+                                SELECT NOW(), 1, @CollectionID, 1, @CollectionObjectID, 1, @PrepTypeID, UUID()";
+                            MySqlCommand cmd = new MySqlCommand(sql, conn);
+                            cmd.Parameters.Add("@CollectionID", MySqlDbType.Int32);
+                            cmd.Parameters.Add("@CollectionObjectID", MySqlDbType.Int32);
+                            cmd.Parameters.Add("@PrepTypeID", MySqlDbType.Int32);
+                            cmd.Parameters["@CollectionID"].Value = collectionID;
+                            cmd.Parameters["@CollectionObjectID"].Value = collectionObjectID;
+                            cmd.Parameters["@PrepTypeID"].Value = prepTypeID;
+                            conn.Open();
+                            int rowsAffected = cmd.ExecuteNonQuery();
+                            conn.Close();
+                            if (rowsAffected > 0)
+                            {
+                                count += rowsAffected;
+                            }
+                        }
+                    }
+                }
+                MessageBox.Show(String.Format("{0} preps inserted", count));
+            }
+            catch (Exception exc)
+            {
+                messageBox.AppendText(exc.ToString() + "\n");
+                MessageBox.Show(String.Format("{0} preps inserted", count));
+            }
+        }
+    }
+}
