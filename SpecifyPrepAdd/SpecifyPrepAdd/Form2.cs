f13abafda0ab4260db121a80b068f5e4c3058e1d
Taylor Wilson <twilson@flmnh.ufl.edu>
2020-09-18 16:17:17 -0400

    Ammeds readme.md and file headers to include license statement

--- SpecifyPrepAdd/SpecifyPrepAdd/Form2.cs
+++ SpecifyPrepAdd/SpecifyPrepAdd/Form2.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/* This software was produced by 
+ * The Florida Museum of Natural History c.2020
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * This work is licensed under a 
+ * Creative Commons Attribution-NonCommercial 4.0 International License.  
+ * http://creativecommons.org/licenses/by-nc/4.0/
+ */
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
